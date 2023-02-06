7a10745f516777e6907d39f9f443133934d56536
JJ Jin <106864282+JIN-ZIJIE@users.noreply.github.com>
2023-02-06 16:23:13 +0800

    Add files via upload

---
+++ api/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, redirect
+
+
+app = Flask(__name__)
+###############################
+############ MAIN #############
+###############################
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+###############################
+############ ERROR ############
+###############################
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
