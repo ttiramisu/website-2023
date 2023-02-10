990f053967c503ad894662a42a20480f56d96457
JJ Jin <106864282+JIN-ZIJIE@users.noreply.github.com>
2023-02-10 18:26:35 +0800

    update

--- api/app.py
+++ api/app.py
@@ -14,20 +14,24 @@ def index():
 @app.route('/main')
 def main():
     return render_template('main.html')
+
+
 ###############################
 ############ ERROR ############
 ###############################
 
-
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+	return render_template("404.html"), 404
 
 
 @app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'), 500
+def page_not_found(e):
+	return render_template("500.html"), 500
 
 
+###############################
+######## RUN TIME CODE ########
+###############################
 if __name__ == "__main__":
     app.run(debug=True)
