6a66d84e3e5d7415017f5badb2c76c4f5e39b870
salted-caramel <105400878+salted-caramel@users.noreply.github.com>
2023-02-10 08:42:55 +0800

    another shit

--- api/static/js/script.js
+++ api/static/js/script.js
@@ -3,10 +3,11 @@
 console.log('bull crap shit')
 
 function scrollInto(id) {
-    var elmnt = document.getElementById(id);
+    var elmnts = document.getElementById(id);
     elmnt.scrollIntoView({
         behavior: "smooth", 
         block: "start",
         inline: "nearest"
     });
+    elmnt.style.marginTop = "10px";
   }
