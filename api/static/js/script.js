1f2f2a394879ffac3c72156d7dec924a9b0084a7
salted-caramel <105400878+salted-caramel@users.noreply.github.com>
2023-02-10 08:39:30 +0800

    gay shit

--- api/static/js/script.js
+++ api/static/js/script.js
@@ -1,3 +1,12 @@
 'use strict'
 
 console.log('bull crap shit')
+
+function scrollInto(id) {
+    var elmnt = document.getElementById(id);
+    elmnt.scrollIntoView({
+        behavior: "smooth", 
+        block: "start",
+        inline: "nearest"
+    });
+  }
