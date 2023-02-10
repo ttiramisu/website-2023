1d7e37153b2ba90baf285a6ded30a1923d9f8649
JJ Jin <106864282+JIN-ZIJIE@users.noreply.github.com>
2023-02-10 18:17:21 +0800

    made 400 and 500 error pages

--- api/app.py
+++ api/app.py
@@ -19,14 +19,14 @@ def main():
 ###############################
 
 
-# @app.errorhandler(404)
-# def page_not_found(e):
-#     return render_template('404.html'), 404
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
-# @app.errorhandler(500)
-# def page_not_found(e):
-#     return render_template('500.html'), 500
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
 
 
 if __name__ == "__main__":
