95b7e2b8f34ed7cf642ccd2a674bd7781f90fe47
salted-caramel <105400878+salted-caramel@users.noreply.github.com>
2023-02-10 08:46:19 +0800

    another one

--- api/static/js/script.js
+++ api/static/js/script.js
@@ -3,7 +3,7 @@
 console.log('bull crap shit')
 
 function scrollInto(id) {
-    var elmnts = document.getElementById(id);
+    var elmnt = document.getElementById(id);
     elmnt.scrollIntoView({
         behavior: "smooth", 
         block: "start",
