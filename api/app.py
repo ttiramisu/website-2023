f80260a4dcf9addbcda046be27194829852b9cc9
JJ Jin <106864282+JIN-ZIJIE@users.noreply.github.com>
2023-02-13 12:27:38 +0800

    psuh

--- api/app.py
+++ api/app.py
@@ -15,6 +15,10 @@ def index():
 def main():
     return render_template('main.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 
 ###############################
 ############ ERROR ############
