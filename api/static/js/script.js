1394f0997c41184fd09602e4f5e7ab9ecd04c813
JJ Jin <106864282+JIN-ZIJIE@users.noreply.github.com>
2023-02-18 19:01:48 +0800

    tested new line

--- api/static/js/script.js
+++ api/static/js/script.js
@@ -2,5 +2,5 @@
 
 console.log(`%cWARNING!`, 'color: red; background-color: yellow; font-size: 2rem; font-family: sans-serif')
 
-console.log(`%cUsing this console may allow attackers to impersonate you and steal your information using an attack called Self-XSS.`, 'font-size: 1.2rem; font-family: monospace;')
-console.log(`%cDo not enter or paste code that you don\'t understand.`, 'font-size: 1.2rem; font-family: monospace;')
+console.log(`%cUsing this console may allow attackers to impersonate you and steal your information using an attack called Self-XSS. \n Do not enter or paste code that you don\'t understand.`, 'font-size: 1.2rem; font-family: monospace;')
+// console.log(`%cDo not enter or paste code that you don\'t understand.`, 'font-size: 1.2rem; font-family: monospace;')
