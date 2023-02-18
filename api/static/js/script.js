4da0652e4affe207b851709fa3b9785eec3e4f8c
JJ Jin <106864282+JIN-ZIJIE@users.noreply.github.com>
2023-02-18 18:58:59 +0800

    update to console

--- api/static/js/script.js
+++ api/static/js/script.js
@@ -1,3 +1,6 @@
 'use strict'
 
-console.log(`%cWARNING!`, 'color: red; background-color: red; font-size: 2.5rem; font-family: "Roboto", sans-serif;')
+console.log(`%cWARNING!`, 'color: red; background-color: yellow; font-size: 2rem; font-family: sans-serif')
+
+console.log(`%cUsing this console may allow attackers to impersonate you and steal your information using an attack called Self-XSS.`, 'font-size: 2rem; font-family: monospace;')
+console.log(`Do not enter or paste code that you don\'t understand.`, 'font-size: 2rem; font-family: monospace;')
