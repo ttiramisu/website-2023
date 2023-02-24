63c06b7310d6afcc50006913e4792c021ff875a5
JJ Jin <106864282+JIN-ZIJIE@users.noreply.github.com>
2023-02-24 22:44:35 +0800

    update

--- api/app.py
+++ api/app.py
@@ -11,10 +11,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
 @app.route('/about')
 def about():
     return render_template('about.html')
-
 
 ###############################
 ############ ERROR ############
