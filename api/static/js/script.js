ccef8952dd91467f3a2eadf5c54c20e5cd2af396
JJ Jin <106864282+JIN-ZIJIE@users.noreply.github.com>
2023-02-27 12:58:13 +0800

    update

--- api/static/js/script.js
+++ api/static/js/script.js
@@ -5,14 +5,16 @@ console.log(`%cWARNING!`, 'color: red; background-color: yellow; font-size: 2rem
 console.log(`%cUsing this console may allow attackers to impersonate you and steal your information using an attack called Self-XSS. \nDo not enter or paste code that you don\'t understand.`, 'font-size: 1.2rem; font-family: monospace; font-weight: 300;')
 // console.log(`%cDo not enter or paste code that you don\'t understand.`, 'font-size: 1.2rem; font-family: monospace;')
 
-// let nav = document.getElementById('nav');
-// window.onscroll = function () {
-//   if (document.body.scrollTop >= 200) {
-//     nav.classList.add("navbar-expand-dark");
-//     nav.classList.remove("nav-transparent");
-//   }
-//   else {
-//     nav.classList.add("nav-transparent");
-//     nav.classList.remove("nav-colored");
-//   }
-// };
+let nav = document.getElementById('nav');
+window.onscroll = function () {
+  if (document.body.scrollTop >= 200) {
+    nav.classList.add("navbar-dark");
+    nav.classList.add("bg-dark");
+    nav.classList.remove("navbar-expand-lg");
+  }
+  else {
+    nav.classList.add("navbar-expand-lg");
+    nav.classList.remove("navbar-dark");
+    nav.classList.remove("bg-dark");
+  }
+};
