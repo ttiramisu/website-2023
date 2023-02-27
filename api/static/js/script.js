3a5fd76dc8a1d693a74a322cb44b68e97065aac0
JJ Jin <106864282+JIN-ZIJIE@users.noreply.github.com>
2023-02-27 12:53:38 +0800

    update

--- api/static/js/script.js
+++ api/static/js/script.js
@@ -4,3 +4,15 @@ console.log(`%cWARNING!`, 'color: red; background-color: yellow; font-size: 2rem
 
 console.log(`%cUsing this console may allow attackers to impersonate you and steal your information using an attack called Self-XSS. \nDo not enter or paste code that you don\'t understand.`, 'font-size: 1.2rem; font-family: monospace; font-weight: 300;')
 // console.log(`%cDo not enter or paste code that you don\'t understand.`, 'font-size: 1.2rem; font-family: monospace;')
+
+// let nav = document.getElementById('nav');
+// window.onscroll = function () {
+//   if (document.body.scrollTop >= 200) {
+//     nav.classList.add("navbar-expand-dark");
+//     nav.classList.remove("nav-transparent");
+//   }
+//   else {
+//     nav.classList.add("nav-transparent");
+//     nav.classList.remove("nav-colored");
+//   }
+// };
