06d35d879524895d5cd1f046265fb93e9628a213
JJ Jin <106864282+JIN-ZIJIE@users.noreply.github.com>
2023-02-28 07:40:12 +0800

    update

--- api/static/js/script.js
+++ api/static/js/script.js
@@ -4,3 +4,7 @@ console.log(`%cWARNING!`, 'color: red; background-color: yellow; font-size: 2rem
 
 console.log(`%cUsing this console may allow attackers to impersonate you and steal your information using an attack called Self-XSS. \nDo not enter or paste code that you don\'t understand.`, 'font-size: 1.2rem; font-family: monospace; font-weight: 300;')
 // console.log(`%cDo not enter or paste code that you don\'t understand.`, 'font-size: 1.2rem; font-family: monospace;')
+
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
