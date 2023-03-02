08ad4951e853bc7fd049b452101ae12c2288fc3f
JJ Jin <106864282+JIN-ZIJIE@users.noreply.github.com>
2023-03-02 13:20:04 +0800

    test

--- api/app.py
+++ api/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', static_url_path='')
 
 ###############################
 ############ MAIN #############
