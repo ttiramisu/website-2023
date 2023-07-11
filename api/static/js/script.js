8c7c0ffececa19f9a0cd65f2162e7a95f70c073d
salted-caramel <105400878+salted-caramel@users.noreply.github.com>
2023-07-11 18:53:01 +0800

    scrolllll

--- api/static/js/script.js
+++ api/static/js/script.js
@@ -1,8 +1,14 @@
-'use strict'
+"use strict";
 
-console.log(`%cWARNING!`, 'color: red; background-color: yellow; font-size: 2rem; font-family: sans-serif')
+console.log(
+  `%cWARNING!`,
+  "color: red; background-color: yellow; font-size: 2rem; font-family: sans-serif"
+);
 
-console.log(`%cUsing this console may allow attackers to impersonate you and steal your information using an attack called Self-XSS. \nDo not enter or paste code that you don\'t understand.`, 'font-size: 1.2rem; font-family: monospace; font-weight: 300;')
+console.log(
+  `%cUsing this console may allow attackers to impersonate you and steal your information using an attack called Self-XSS. \nDo not enter or paste code that you don\'t understand.`,
+  "font-size: 1.2rem; font-family: monospace; font-weight: 300;"
+);
 // console.log(`%cDo not enter or paste code that you don\'t understand.`, 'font-size: 1.2rem; font-family: monospace;')
 
 const yearEl = document.querySelector(".year");
@@ -10,12 +16,22 @@ const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
 window.onscroll = () => {
-  progress()
+  progress();
 };
 
 const progress = () => {
-  const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  const winScroll =
+    document.body.scrollTop || document.documentElement.scrollTop;
+  const height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
   const scrolled = (winScroll / height) * 100;
   document.getElementById("Bar").style.width = scrolled + "%";
+};
+
+function scrollToSection(sectionId) {
+  const section = document.getElementById(sectionId);
+  if (section) {
+    section.scrollIntoView({ behavior: "smooth" });
+  }
 }
