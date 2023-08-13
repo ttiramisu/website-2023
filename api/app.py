072b589aedb89b61ddc7afce6efef8f590a87ce9
ttiramisu <106864282+ttiramisu@users.noreply.github.com>
2023-08-13 21:18:46 +0800

    removed access to 404 and 500 pages

--- api/app.py
+++ api/app.py
@@ -95,13 +95,13 @@ def page_not_found(e):
 def page_not_found(e):
 	return render_template("500.html"), 500
 
-@app.route('/404')
-def error_404():
-    return render_template('404.html')
+# @app.route('/404')
+# def error_404():
+#     return render_template('404.html')
 
-@app.route('/500')
-def error_500():
-    return render_template('500.html')
+# @app.route('/500')
+# def error_500():
+#     return render_template('500.html')
 
 
 ###############################
