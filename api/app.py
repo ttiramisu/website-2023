0ad1217e197bbfa93c58c8a5cc0d7d5bac8a5d49
ttiramisu <106864282+ttiramisu@users.noreply.github.com>
2023-08-19 20:06:42 +0800

    oush

--- api/app.py
+++ api/app.py
@@ -100,7 +100,7 @@ def page_not_found(e):
 ###############################
 @app.route('/thanks')
 def thanks():
-    return render_template('easter/thanks.txt')
+    return render_template('easter/thanks.html')
 
 ###############################
 ######## RUN TIME CODE ########
