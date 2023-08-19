2d1b9c6b5a5e3c8b5c0b4b1b8b572c1a591a7253
ttiramisu <106864282+ttiramisu@users.noreply.github.com>
2023-08-19 19:54:19 +0800

    updates

--- api/app.py
+++ api/app.py
@@ -95,14 +95,12 @@ def page_not_found(e):
 def page_not_found(e):
 	return render_template("500.html"), 500
 
-# @app.route('/404')
-# def error_404():
-#     return render_template('404.html')
-
-# @app.route('/500')
-# def error_500():
-#     return render_template('500.html')
-
+###############################
+########### EASTER ############
+###############################
+@app.route('/thanks')
+def thanks():
+    return render_template('thanks.txt')
 
 ###############################
 ######## RUN TIME CODE ########
